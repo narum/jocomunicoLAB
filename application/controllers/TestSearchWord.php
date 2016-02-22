f6b30bc6de6b85cdc077e7e8c7034b6de2c91555
Jordi <Jordi@158.109.126.11>
2016-02-22 11:29:21 +0100

    Final Search Model 22-02-2016

--- application/controllers/TestSearchWord.php
+++ application/controllers/TestSearchWord.php
@@ -29,7 +29,7 @@ class TestSearchWord extends REST_Controller {
         if ($languageNum == 1){
             $language = "ca";
         } else if($languageNum == 2) {
-            $language = "es";
+            $language = "ES";
         }
          
         // Concatenamos el nombre del pictograma con la ruta del archivo
@@ -40,47 +40,8 @@ class TestSearchWord extends REST_Controller {
         }
 
         // Llamamos al modelo
-        $names = $this->DBwords->getDBVerbsLike($startswith, $language);
-        $names2 = $this->DBwords->getDBNamesLike($startswith, $language);
-        $names = $names + $names2;
-        $response = [
-            "data" => array_map("concat_path", $names)
-        ];
-        
-        $this->response($response, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
-
-    }
-    public function getDBAll()
-    {
-        $startswith = $this->query("startswith");
-        $languageNum = $this->query("language");
-        
-        $startswith = "a";
-        $languageNum = 2;
-
-        // Comprobamos si llegan parametros del get   
-        if($startswith == NULL || $startswith == "") {
-            $this->response("missing argument startswith", 400);
-            return;
-        }
-
-        // guardamos como "ca" el lenguaje "1" y com "es" el lenguaje "2"
-        if ($languageNum == 1){
-            $language = "ca";
-        } else if($languageNum == 2) {
-            $language = "es";
-        }
-         
-        // Concatenamos el nombre del pictograma con la ruta del archivo
-        function concat_path($row) {
-            $newPath = base_url() . "img/pictos/" . $row["imgPicto"];
-            $row["imgPicto"] = $newPath;
-            return $row;
-        }
-
-        // Llamamos al modelo
-        $names = $this->DBwords->getDBVerbsLike($startswith, $language);
-        $names2 = $this->DBwords->getDBNamesLike($startswith, $language);
+        $names = $this->DBwords->getDBModifsLike($startswith, $language);
+        $names2 = $this->DBwords->getDBQuestionPartLike($startswith, $language);
         $names = $names + $names2;
         $response = [
             "data" => array_map("concat_path", $names)
